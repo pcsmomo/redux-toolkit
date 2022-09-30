2eeb9521f814213424e5f8d5d6b0a594a7d6d5d2
Noah Kim <dongwoo_333@hanmail.net>
2022-09-30 10:38:35 +1000

    add CUD

--- rtk-query-basic/src/features/api/apiSlice.js
+++ rtk-query-basic/src/features/api/apiSlice.js
@@ -7,7 +7,33 @@ export const apiSlice = createApi({
     getTodos: builder.query({
       query: () => "/todos",
     }),
+    addTodo: builder.mutation({
+      query: (todo) => ({
+        url: "/todos",
+        method: "POST",
+        body: todo,
+      }),
+    }),
+    updateTodo: builder.mutation({
+      query: (todo) => ({
+        url: `/todos/${todo.id}`,
+        method: "PATCH",
+        body: todo,
+      }),
+    }),
+    deleteTodo: builder.mutation({
+      query: ({ id }) => ({
+        url: `/todos/${id}`,
+        method: "DELETE",
+        body: id,
+      }),
+    }),
   }),
 });
 
-export const { useGetTodosQuery } = apiSlice;
+export const {
+  useGetTodosQuery,
+  useAddTodoMutation,
+  useUpdateTodoMutation,
+  useDeleteTodoMutation,
+} = apiSlice;
