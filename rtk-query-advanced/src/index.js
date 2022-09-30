5959f3bc2fb50f9965d9bdbc393344c141f129d9
Noah Kim <dongwoo_333@hanmail.net>
2022-09-30 13:56:51 +1000

    Redux Advanced Tutorial - React, Redux Toolkit, RTK Query Project: init

--- rtk-query-advanced/src/index.js
+++ rtk-query-advanced/src/index.js
@@ -1,10 +1,25 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import { store } from "./app/store";
+import { Provider } from "react-redux";
+import { fetchPosts } from "./features/posts/postsSlice";
+import { fetchUsers } from "./features/users/usersSlice";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+store.dispatch(fetchPosts());
+store.dispatch(fetchUsers());
+
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Provider store={store}>
+      <Router>
+        <Routes>
+          <Route path="/*" element={<App />} />
+        </Routes>
+      </Router>
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
