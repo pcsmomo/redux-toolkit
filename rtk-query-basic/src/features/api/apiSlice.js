fa48bea4c23142843f53f68ca5b502292d06fe40
Noah Kim <dongwoo_333@hanmail.net>
2022-09-30 10:42:28 +1000

    apply tags to reflect cashed data when transaction occurs

--- rtk-query-basic/src/features/api/apiSlice.js
+++ rtk-query-basic/src/features/api/apiSlice.js
@@ -3,9 +3,11 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3500" }),
+  tagTypes: ["Todos"],
   endpoints: (builder) => ({
     getTodos: builder.query({
       query: () => "/todos",
+      providesTags: ["Todos"],
     }),
     addTodo: builder.mutation({
       query: (todo) => ({
@@ -13,6 +15,7 @@ export const apiSlice = createApi({
         method: "POST",
         body: todo,
       }),
+      invalidatesTags: ["Todos"],
     }),
     updateTodo: builder.mutation({
       query: (todo) => ({
@@ -20,6 +23,7 @@ export const apiSlice = createApi({
         method: "PATCH",
         body: todo,
       }),
+      invalidatesTags: ["Todos"],
     }),
     deleteTodo: builder.mutation({
       query: ({ id }) => ({
@@ -27,6 +31,7 @@ export const apiSlice = createApi({
         method: "DELETE",
         body: id,
       }),
+      invalidatesTags: ["Todos"],
     }),
   }),
 });
